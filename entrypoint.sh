332284f621c877b2549b9c045efbb1b3188527c3
Christophe Pruvost <christophe.pruvost@oracle.com>
2022-06-27 15:50:53 +0200

    test env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -l
 
-printenv
-echo "OCI User $INPUT_USER"
+##printenv
+echo "::group::Input Variables"
+echo "User $INPUT_USER"
+echo "Fingerprint $INPUT_FINGERPRINT"
+echo "Tenancy $INPUT_TENANCY"
+echo "Region $INPUT_REGION"
+echo "API Key $INPUT_API_KEY"
+echo "::endgroup::"
+
 time=$(date)
 echo ::set-output name=time::$time
