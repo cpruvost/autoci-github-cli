7f959a90dc1d6e24c87ee907c2cb087de733ceef
Christophe Pruvost <christophe.pruvost@oracle.com>
2022-06-27 16:50:32 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,17 @@ echo "Region $INPUT_REGION"
 echo "API Key $INPUT_API_KEY"
 ##echo "::endgroup::"
 
+##echo "::group::Install OCI CLI"
 curl -L -O https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh
 chmod +x install.sh
 ./install.sh --accept-all-defaults
+##echo "::endgroup::"
+
+##echo "::group::Create OCI config file"
+echo "[default]" > ~/.oci/config
+echo "user=$INPUT_USER" >> ~/.oci/config
+cat ~/.oci/config
+##echo "::endgroup::"
 
 time=$(date)
 echo ::set-output name=time::$time
